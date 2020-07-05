4012ec88eebbab7eff8f59acc4686bc5c6c2c87e
ankit.soni@quillbot.com <gourav.jhangihel@gmail.com>
2020-07-05 23:01:16 +0530

    Add multiple Chart

--- src/Login.jsx
+++ src/Login.jsx
@@ -32,15 +32,142 @@ class Login extends Component {
             }
         });
     }
+    showAreaChart() {
+        $("#grid").kendoChart({
+            title: {
+                text: "Gross domestic product growth \n /GDP annual %/"
+            },
+            legend: {
+                position: "bottom"
+            },
+            seriesDefaults: {
+                type: "area",
+                area: {
+                    line: {
+                        style: "smooth"
+                    }
+                }
+            },
+            series: [{
+                name: "India",
+                data: [3.907, 7.943, 7.848, 9.284, 9.263, 9.801, 3.890, 8.238, 9.552, 6.855]
+            }, {
+                name: "World",
+                data: [1.988, 2.733, 3.994, 3.464, 4.001, 3.939, 1.333, -2.245, 4.339, 2.727]
+            }, {
+                name: "Haiti",
+                data: [-0.253, 0.362, -3.519, 1.799, 2.252, 3.343, 0.843, 2.877, -5.416, 5.590]
+            }],
+            valueAxis: {
+                labels: {
+                    format: "{0}%"
+                },
+                line: {
+                    visible: false
+                },
+                axisCrossingValue: -10
+            },
+            categoryAxis: {
+                categories: [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011],
+                majorGridLines: {
+                    visible: false
+                },
+                labels: {
+                    rotation: "auto"
+                }
+            },
+            tooltip: {
+                visible: true,
+                format: "{0}%",
+                template: "#= series.name #: #= value #"
+            }
+        });
+    }
+    showBarChart() {
+        $("#grid").kendoChart({
+            title: {
+                text: "Kendo Chart Example"
+            },
+            series: [{
+                type: "bar",
+                name: "Example Series",
+                data: [200, 450, 300, 125]
+            }],
+            categoryAxis: {
+                categories: [2000, 2001, 2002, 2003]
+            }
+        });
+    }
+    showBubbleChart() {
+        var salesData = [{
+            numberOfSales: 10,
+            volume: 12000,
+            marketShare: 0.1
+        }, {
+            numberOfSales: 24,
+            volume: 62000,
+            marketShare: 0.25
+        }];
+
+        $("#grid").kendoChart({
+            dataSource: {
+                data: salesData
+            },
+            series: [{
+                name: "Sales",
+                type: "bubble",
+                xField: "numberOfSales",
+                yField: "volume",
+                sizeField: "marketShare"
+            }]
+        });
+    }
+    showDifferentThemeChart() {
+        $("#grid").kendoChart({
+            title: {
+                text: "Internet Users"
+            },
+            legend: {
+                position: "bottom"
+            },
+            panes: [
+                { name: "top-pane", background: "black" },
+            ],
+            seriesDefaults: {
+                type: "line"
+            },
+            series: [{
+                name: "World",
+                data: [15.7, 16.7, 20, 23.5, 26.6]
+            }, {
+                name: "United States",
+                data: [67.96, 68.93, 75, 74, 78]
+            }],
+            valueAxis: {
+                labels: {
+                    format: "{0}%"
+                }
+            },
+            categoryAxis: {
+                categories: [2005, 2006, 2007, 2008, 2009]
+            }
+        });
+    }
     render() {
         return (
             <div>
                 <header className="header">
                     <nav className="navbar navbar-default">
                         <div className="container-fluid">
-                            <div className="navbar-header">
-                                <a className="navbar-brand" href="#">Sales</a>
+                            <div className="col-md-6 col-xs-12">
+                                <div className="navbar-header">
+                                    <a className="navbar-brand" href="#">Sales</a>
+                                </div>
                             </div>
+                            <div className="col-md-6 col-xs-12 text-right">
+                                <a className="btn btn-default">Send</a>
+                            </div>
+
 
                         </div>
                     </nav>
@@ -68,9 +195,10 @@ class Login extends Component {
                                 <div className="tab-content">
 
                                     <div className="tabbtns text-right">
-                                        <a className="btn btn-default">YTD</a>
-                                        <a className="btn btn-default">QTD</a>
-                                        <a className="btn btn-default">last 30 days</a>
+                                        <a className="btn btn-default" onClick={this.showAreaChart}>YTD</a>
+                                        <a className="btn btn-default" onClick={this.showDifferentThemeChart}>WTD</a>
+                                        <a className="btn btn-default" onClick={this.showBarChart}>QTD</a>
+                                        <a className="btn btn-default" onClick={this.showBubbleChart}>last 30 days</a>
                                     </div>
 
                                     <div id="home" className="tab-pane fade in active">
